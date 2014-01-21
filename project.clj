837fa701e24516aebcdd97e2f1595f34dea7a884
Paul Stadig <paul@stadig.name>
2014-01-21 09:28:40 -0500

    Bump to 0.2.0.

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject pjstadig/es "0.2.0-SNAPSHOT"
+(defproject pjstadig/es "0.2.0"
   :description "A simple REST-based Elasticsearch client for Clojure."
   :url "http://github.com/pjstadig/es"
   :license {:name "Mozilla Public License, v. 2.0"
             :url "http://mozilla.org/MPL/2.0/"}
+  :plugins [[codox "0.6.6"]]
   :dependencies [[cheshire "5.2.0"]
                  [clj-http "0.7.7"]
                  [log4j "1.2.17"]
